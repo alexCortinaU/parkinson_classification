1e5f7c02413824b5f46a36c269f2c21418defcc8
Alejandro Cortina <acortinau@gmail.com>
2023-03-17 13:25:36 +0100

    update dataset for patches

--- dataset/hmri_dataset.py
+++ dataset/hmri_dataset.py
@@ -79,6 +79,8 @@ class HMRIDataModule(pl.LightningDataModule):
         # split ratio train = 0.6, val = 0.2, test = 0.2
 
         # drop subject 058 because it doesn't have maps
+        # 'sub-016' has PD* map completely black
+        # sub-025 has no brain mask
         subjs_to_drop = ['sub-058', 'sub-016']
         if self.brain_masked:
             subjs_to_drop.append('sub-025')
@@ -185,6 +187,166 @@ class HMRIDataModule(pl.LightningDataModule):
         return DataLoader(self.test_set, 
                             self.val_batch_size, 
                             num_workers=self.val_num_workers,
+                            shuffle=False)
+
+    def predict_dataloader(self):
+        return DataLoader(self.val_set, 
+                            self.val_batch_size, 
+                            num_workers=self.val_num_workers,
+                            shuffle=False)
+
+class HMRIControlsDataModule(pl.LightningDataModule):
+    def __init__(self, 
+                md_df, 
+                root_dir,
+                train_batch_size = 4,
+                val_batch_size = 4,
+                train_num_workers = 4,
+                val_num_workers = 4, 
+                reshape_size = (128, 128, 128), 
+                patch_size = (64, 64, 64),
+                map_type = ['MTsat'],
+                queue_length = 20,
+                samples_per_volume = 9,
+                test_split = 0.3, 
+                random_state = 42,
+                windowed_dataset = False,
+                brain_masked = False,
+                augment = None,
+                shuffle = True):
+        super().__init__()
+        self.md_df = md_df
+        self.train_batch_size = train_batch_size
+        self.val_batch_size = val_batch_size
+        self.train_num_workers = train_num_workers
+        self.val_num_workers = val_num_workers
+        self.root_dir = root_dir
+        self.reshape_size = reshape_size
+        self.patch_size = patch_size
+        self.map_type = map_type
+        self.queue_length = queue_length
+        self.samples_per_volume = samples_per_volume
+        self.test_split = test_split
+        self.random_state = random_state
+        self.windowed_dataset = windowed_dataset
+        self.brain_masked = brain_masked
+        self.augment = augment
+        self.shuffle = shuffle
+        self.subjects = None
+        self.test_subjects = None
+        self.preprocess = None
+        self.transform = None
+        self.train_set = None
+        self.val_set = None
+        self.test_set = None
+
+        self.num_channels = len(self.map_type)
+
+    def get_subjects_list(self, md_df):
+        subjects_list = []
+  
+        md_df.reset_index(inplace=True, drop=True)
+        for i in range(len(md_df)):
+            subj_dir = self.root_dir / md_df['id'][i] / 'Results'
+            if self.windowed_dataset:
+                if self.brain_masked:
+                    hmri_files = sorted(list(subj_dir.glob('*w_masked.nii')), key=lambda x: x.stem)
+                else:
+                    hmri_files = sorted(list(subj_dir.glob('*_w.nii')), key=lambda x: x.stem)
+            else:
+                hmri_files = sorted(list(subj_dir.glob('*.nii')), key=lambda x: x.stem)
+                hmri_files = [file for file in hmri_files if '_w' not in file.stem]
+
+            hmri_files = [x for x in hmri_files if any(sub in x.stem for sub in self.map_type)]
+            subjects_list.append(hmri_files)
+
+
+        return subjects_list
+    
+    def prepare_data(self):
+
+        # Reset index        
+        self.md_df.reset_index(drop=True, inplace=True)
+
+        # Split dataset into train and val, stratified by sex
+        self.md_df_train, self.md_df_val = train_test_split(self.md_df, test_size=self.test_split,
+                                                random_state=self.random_state, stratify=self.md_df.loc[:, 'sex'].values)
+                                                
+        image_training_paths = self.get_subjects_list(self.md_df_train)
+        image_val_paths = self.get_subjects_list(self.md_df_val)
+
+        self.train_subjects = []
+        for image_path in image_training_paths:
+            # 'image' and 'label' are arbitrary names for the images
+            subject = tio.Subject(image=tio.ScalarImage(image_path))
+            self.train_subjects.append(subject)
+
+        self.val_subjects = []
+        for image_path in image_val_paths:
+            # 'image' and 'label' are arbitrary names for the images
+            subject = tio.Subject(image=tio.ScalarImage(image_path))
+            self.val_subjects.append(subject)
+
+    def get_preprocessing_transform(self):
+        # Rescales intensities to [0, 1] and adds a channel dimension,
+        # then resizes to the desired shape
+
+        # preprocess = Compose(
+        #     [ScaleIntensity(), 
+        #     EnsureChannelFirst(), 
+        #     ResizeWithPadOrCrop(self.reshape_size)
+        #     ])
+        preprocess = tio.Compose(
+            [   tio.ToCanonical(),
+                tio.RescaleIntensity((0, 1)),
+                tio.CropOrPad(self.reshape_size, 
+                              padding_mode='minimum')
+            ]
+        )
+        return preprocess
+
+    def get_augmentation_transform(self):
+
+        # If no augmentation is specified, use the default one
+        if self.augment == None:
+            self.augment = tio.Compose([])
+
+    def setup(self, stage=None):
+        
+        # Assign train/val datasets for use in dataloaders
+        self.preprocess = self.get_preprocessing_transform()
+        self.get_augmentation_transform()
+        self.transform = tio.Compose([self.preprocess, self.augment])
+
+        self.train_set = tio.SubjectsDataset(self.train_subjects, transform=self.transform)
+        self.val_set = tio.SubjectsDataset(self.val_subjects, transform=self.preprocess)
+
+    def train_dataloader(self):
+        
+        sampler = tio.data.UniformSampler(self.patch_size)
+        patches_queue = tio.Queue(self.train_set,
+                                  self.queue_length,
+                                  self.samples_per_volume,
+                                  sampler,
+                                  num_workers=self.train_num_workers,
+                                  shuffle_subjects=self.shuffle,
+                                  shuffle_patches=self.shuffle)
+        return DataLoader(patches_queue, 
+                            self.train_batch_size, 
+                            num_workers=0)
+
+    def val_dataloader(self):
+        sampler = tio.data.UniformSampler(self.patch_size)
+        patches_queue = tio.Queue(self.val_set,
+                                  self.queue_length,
+                                  self.samples_per_volume,
+                                  sampler,
+                                  num_workers=self.val_num_workers,
+                                  shuffle_subjects=False,
+                                  shuffle_patches=False)
+        return DataLoader(patches_queue, 
+                            self.train_batch_size, 
+                            num_workers=0,
                             shuffle=False)
 
     def predict_dataloader(self):
